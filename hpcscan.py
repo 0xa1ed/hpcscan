a178474e5c368c2d92c42d858e6b390d0ebcbb3c
0xa1ed <0xa1ed@riseup.net>
2015-04-15 23:19:55 +0100

    updating update function

--- hpcscan.py
+++ hpcscan.py
@@ -8,7 +8,7 @@ import os, sys, subprocess, getopt, profiles
 #from profiles import *
 
 def update():
-    subprocess.call(['./extensions/upgrade.sh'])
+    subprocess.call('upgrade.sh', cwd='~/hpcscan/extensions/')
     sys.exit(0)
 
 def main(argv):
