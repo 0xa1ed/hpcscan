0a28d4de397d31149f17ffad205ff18884ab2c74
0xa1ed <0xa1ed@riseup.net>
2015-04-15 17:47:36 +0100

    added license to hpcscan.py

--- hpcscan.py
+++ hpcscan.py
@@ -1,3 +1,9 @@
+# hpcscan.py
+# Author: Aled Rhys Evans <evansar6@cardiff.ac.uk>
+# License: Simplified BSD License (FreeBSD License) 
+# Description: An utility to look at Capture-HPC logs and determine if a particular set of logs
+#              displays malicious actions by comparing it to profiles of logs already determined
+#              to be malicious. 
 import os, sys, profiles
 from profiles import *
 
