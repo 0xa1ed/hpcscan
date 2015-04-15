d381d2be8e2dc959e389581f9879a6bbb9ef0698
0xa1ed <0xa1ed@riseup.net>
2015-04-15 23:09:41 +0100

    added update feature

--- hpcscan.py
+++ hpcscan.py
@@ -4,15 +4,35 @@
 # Description: An utility to look at Capture-HPC logs and determine if a particular set of logs
 #              displays malicious actions by comparing it to profiles of logs already determined
 #              to be malicious. This is under active development and is not for production use yet. 
-import os, sys, profiles
+import os, sys, subprocess, getopt, profiles
 #from profiles import *
 
-print "Modules loaded:",
-for module in profiles.__all__:
-    print module,
+def update():
+    subprocess.call(['./extensions/upgrade.sh'])
+    sys.exit(0)
 
-print ""
+def main(argv):
 
-for module in profiles.__all__:
-    exec '%s = profiles.%s.%s()' % (module, module, module)
-    exec 'print %s.greet' % module
+    try:
+        opts, args = getopt.getopt("u", ["update"])
+    except getopt.GetoptError:
+        print "Error. Exiting."
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-u", "--update"):
+            update()
+
+
+    print "Modules loaded:",
+    for module in profiles.__all__:
+        print module,
+
+    print ""
+
+    for module in profiles.__all__:
+        exec '%s = profiles.%s.%s()' % (module, module, module)
+        exec 'print %s.greet' % module
+
+main(sys.argv)
+update()
