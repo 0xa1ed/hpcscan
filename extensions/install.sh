1effda47ac22d41912d694e8af15df99118619c6
0xa1ed <0xa1ed@riseup.net>
2015-04-15 20:13:29 +0100

    changed license and added install script

---
+++ extensions/install.sh
@@ -0,0 +1,10 @@
+set -e
+
+if [ ! -n "$HPCSCAN" ]; then
+        HPCSCAN=~/hpcscan
+fi 
+
+hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/0xa1ed/hpcscan.git $HPCSCAN || {
+  echo "please install git"
+    exit
+    }
