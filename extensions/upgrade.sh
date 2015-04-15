77f5e20a6cd7ecfa5a36e3b5e0fe5656a9205177
0xa1ed <0xa1ed@riseup.net>
2015-04-15 20:39:56 +0100

    added upgrade script

---
+++ extensions/upgrade.sh
@@ -0,0 +1,8 @@
+printf "Upgrading HPCScan.\n"
+cd ~/hpcscan
+if git pull --rebase --stat origin master
+then
+    printf "Update successful.\n"
+else
+    printf "Update failed. Please try again later."
+fi
