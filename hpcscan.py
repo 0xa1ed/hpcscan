48a37517cf5b7917a5dcbbadf513835557c1c082
0xa1ed <0xa1ed@riseup.net>
2015-04-15 20:58:50 +0100

    added explicit import to profiles/__init__.py and removed unnecessary second import function from hpcscan.py

--- hpcscan.py
+++ hpcscan.py
@@ -5,7 +5,7 @@
 #              displays malicious actions by comparing it to profiles of logs already determined
 #              to be malicious. This is under active development and is not for production use yet. 
 import os, sys, profiles
-from profiles import *
+#from profiles import *
 
 print "Modules loaded:",
 for module in profiles.__all__:
@@ -14,5 +14,5 @@ for module in profiles.__all__:
 print ""
 
 for module in profiles.__all__:
-    exec '%s = %s.%s()' % (module, module, module)
+    exec '%s = profiles.%s.%s()' % (module, module, module)
     exec 'print %s.greet' % module
