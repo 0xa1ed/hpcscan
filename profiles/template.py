d9ccfe15bd237f4e71f7935159eaa82dd9bdab67
0xa1ed <0xa1ed@riseup.net>
2015-04-24 13:52:54 +0100

    added template profile

---
+++ profiles/template.py
@@ -0,0 +1,15 @@
+class template:
+    def __init__(self):
+        self.procstart = []
+        self.procterm = []
+        self.regset = []
+        self.regdel = []
+        self.filewrite = []
+        self.filedel = []
+        self.score = 0
+        self.accuracy = 0
+        self.origorder = []
+        self.outbuff = []
+
+    def getacc():
+        return accuracy
---
+++ profiles/unidentified.py
@@ -0,0 +1,16 @@
+class unidentified:
+    def __init__(self):
+        self.procstart = []
+        self.procterm = []
+        self.regset = []
+        self.regdel = []
+        self.filewrite = []
+        self.filedel = []
+        self.score = 0
+        self.accuracy = 0
+        self.origorder = []
+        self.outbuffer = []
+
+    def getacc():
+        return accuracy
+
